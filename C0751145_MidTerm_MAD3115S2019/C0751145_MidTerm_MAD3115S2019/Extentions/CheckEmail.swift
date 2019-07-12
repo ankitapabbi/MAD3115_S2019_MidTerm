bee6c16ee24fd7c09ead417318c31c92a7ab0350
Ankita Pabbi <erankitapabbi@gmail.com>
2019-07-12 19:00:35 -0400

    [Ankita] Extension For Email validation.

---
+++ C0751145_MidTerm_MAD3115S2019/C0751145_MidTerm_MAD3115S2019/Extentions/CheckEmail.swift
@@ -0,0 +1,9 @@
+//
+//  CheckEmail.swift
+//  C0751145_MidTerm_MAD3115S2019
+//
+//  Created by Ankita Pabbi on 2019-07-12.
+//  Copyright © 2019 Ankita Pabbi. All rights reserved.
+//
+
+import Foundation
